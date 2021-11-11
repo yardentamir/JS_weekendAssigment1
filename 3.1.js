b7c493d0df52cf66f08246ed1ebdbdc1e0f3c2a8
yarentamir <yarden.tamir@walla.co.il>
2021-11-11 19:53:01 +0200

    שש

--- 3.1.js
+++ 3.1.js
@@ -1,3 +1,13 @@
 (function () {
   "use strict";
+
+  const IS_FIRST_YEAR = true;
+  const nb_year = (p0, percent, aug, p) => {
+    if (IS_FIRST_YEAR) {
+      IS_FIRST_YEAR = false;
+      p0 + p0 * (percent / 100) + 50;
+    } else p0 + p0 * (percent / 100) + 50;
+  };
+
+  console.log(nb_year(1500, 5, 100, 5000));
 })();
