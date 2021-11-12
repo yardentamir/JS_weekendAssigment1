63e5133fdbcae4f3f2f727addbf9db1d45f4ad22
yarentamir <yarden.tamir@walla.co.il>
2021-11-12 16:26:24 +0200

    5.8

--- 5.7.js
+++ 5.7.js
@@ -5,15 +5,17 @@
     return arr.sort((a, b) => a.length - b.length)[0].length;
   };
 
-  const smallestWords = (wordsArray) => {
-    let arrOfSmallestWords = [];
-    const lengthSmallest = lengthOfSmallestWords(wordsArray);
-    for (let i = 0; i < wordsArray.length; i++) {
-      if (lengthSmallest === wordsArray[i].length)
-        arrOfSmallestWords.push(wordsArray[i]);
-    }
-    return arrOfSmallestWords;
-  };
-
-  console.log(smallestWords(["one", "two", "three", "six", "seven", "ttt"]));
+  console.log(
+    lengthOfSmallestWords([
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ])
+  );
 })();
--- 5.8.js
+++ 5.8.js
@@ -1,3 +1,31 @@
 (function () {
   "use strict";
+
+  const lengthOfLongestWords = (arr) => {
+    return arr.sort((a, b) => b.length - a.length)[0].length;
+  };
+
+  const LongestWords = (wordsArray) => {
+    let arrOfLongestWords = [];
+    const lengthLongest = lengthOfLongestWords(wordsArray);
+    for (let i = 0; i < wordsArray.length; i++) {
+      if (lengthLongest === wordsArray[i].length)
+        arrOfLongestWords.push(wordsArray[i]);
+    }
+    return arrOfLongestWords;
+  };
+
+  console.log(
+    LongestWords([
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ])
+  );
 })();
