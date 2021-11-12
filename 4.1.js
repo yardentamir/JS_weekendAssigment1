aad0d21e6184e7c2dc51dca198af3bd03d16b440
yarentamir <yarden.tamir@walla.co.il>
2021-11-12 11:37:04 +0200

    4.1

--- 4.1.js
+++ 4.1.js
@@ -4,20 +4,15 @@
   const fibonacci = (n, s) => {
     let fib = [],
       pos = 2;
-    if (s === 0) {
-      fib = [0, 1];
-      s += 3;
-    } else {
-      fib = [1, 1];
-      s += 2;
-    }
 
-    for (; s <= n; s++) {
+    s === 0 ? (fib = [0, 1]) : (fib = [1, 1]);
+
+    for (s = 3; s <= n; s++) {
       fib[pos] = fib[pos - 2] + fib[pos - 1];
       pos++;
     }
     return fib;
   };
 
-  console.log(fibonacci(10, 0));
+  console.log(fibonacci(15, 1));
 })();
