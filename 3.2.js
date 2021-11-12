61cd5a6027005085c9fd05ecd8d379ec2dc81c43
yarentamir <yarden.tamir@walla.co.il>
2021-11-12 22:36:41 +0200

    3.2

--- 3.2.js
+++ 3.2.js
@@ -1,20 +1,39 @@
 (function () {
   "use strict";
 
-  const peopleOnTheBus = function (array) {
+  //solution 1
+  const peopleOnTheBus = (array) => {
     let peopleOn = 0;
     let peopleOut = 0;
-    for (let i = 0; i < array.length - 1; i++) {
+    for (let i = 0; i < array.length; i++) {
       peopleOn += array[i][0];
       peopleOut += array[i][1];
     }
     return peopleOn - peopleOut;
   };
 
+  // solution2
+
+  const peopleOnTheBus2 = (array) => {
+    return array.reduce(
+      (previousValue, [inTheBus, outTheBus]) =>
+        previousValue + inTheBus - outTheBus,
+      0
+    );
+  };
+
   console.log(
     peopleOnTheBus([
+      [11, 0],
+      [5, 8],
+      [7, 10],
+    ])
+  );
+
+  console.log(
+    peopleOnTheBus2([
       [12, 0],
-      [5, 4],
+      [10, 4],
       [7, 10],
     ])
   );
