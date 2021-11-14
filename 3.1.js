3b0155e5339ad0c7311be797240b3b8ccd9efae1
yarentamir <yarden.tamir@walla.co.il>
2021-11-14 07:32:12 +0200

    .

--- 3.1.js
+++ 3.1.js
@@ -2,8 +2,7 @@
   "use strict";
 
   const nb_year = (p0, percent, aug, p) => {
-    let i = 0;
-    for (; p0 <= p; i++) p0 = p0 + p0 * (percent / 100) + aug;
+    for (var i = 0; p0 <= p; i++) p0 = p0 + p0 * (percent / 100) + aug;
     return i;
   };
 
